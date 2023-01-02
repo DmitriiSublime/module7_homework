8a244dcf450ceb62cf457ea6ede98cd44342e2a4
Dmitrii <115217175+DmitriiSublime@users.noreply.github.com>
2023-01-02 12:18:25 +0300

    Update task5.js

--- app/task5.js
+++ app/task5.js
@@ -1,32 +1,3 @@
-// class Parent {
-//constructor(ownCity) {
-//this.ownCity = ownCity;
-//this.hasFlat = true;
-   
-//}
-   
-//getInfo(){
-//console.log(`I live in the ${this.ownCity}`)
-   
-//}
-   
-//}
-
-/////////////////
-
-// function Parent (ownCity) {
-//     this.ownCity = ownCity,
-//     this.hasFlat = true;
-   
-//    }
-   
-//    Parent.prototype.getInfo(){
-//     console.log(`I live in the ${this.ownCity}`)
-   
-//    }
-
-// let parent = new Parent('Moscow');
-
 // Задание 5.
 // Переписать консольное приложение из предыдущего юнита на классы.
 // Общие требования:
@@ -34,33 +5,6 @@
 // Соблюдать best practices;
 // Использовать синтаксис ES6.
 
-// function Staff (name, voltage, room){
-//     this.name = name,
-//     this.room = room,
-//     this.voltage = voltage
-//     this.switch = function (){
-//         console.log(name + ' is ON')
-//     }
-//     this.power = function (){
-//         console.log(name + ' voltage is ' + 'W' + voltage)
-//     }
-//     this.location = function (){
-//         console.log(name + ' in ' + room)
-//     }
-    
-// }
-// let laptop = new Staff('Laptop', 80, 'Bedroom');
-// let fridge = new Staff('Fridge', 120, 'Hall');
-// let kettle = new Staff('Kettle', 180, 'Kitchen');
-// let hairDryer = new Staff('Hair dryer', 210, 'Bathroom');
-
-// laptop.location();
-// laptop.power();
-// laptop.switch();
-
-// fridge.location();
-// fridge.power();
-// fridge.switch();
 
 
 class Staff {
@@ -110,6 +54,3 @@ console.log(forHair.getInfo);
 console.log(forHair.getLocation);
 console.log(forHair.getPower);
 console.log(forHair.getPrice);
-
-
-
